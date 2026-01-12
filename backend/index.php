a425f35d28522c69620302a8aebdae951e40b931
Logaraman <logaraman2403@gmail.com>
2026-01-12 14:06:39 +0530

    Add CORS headers for Netlify frontend

--- backend/index.php
+++ backend/index.php
@@ -1,6 +1,16 @@
 <?php
-header("Content-Type: application/json");
+// CORS headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+// Handle preflight
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+header("Content-Type: application/json");
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch ($uri) {
